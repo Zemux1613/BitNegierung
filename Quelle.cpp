8b1fac6a916416e654cef75caa9ac091bf37e9ef
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2021-10-19 10:21:33 +0200

    feat: add file

---
+++ Quelle.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+	// 2er Komplement
+
+	printf("Bitte gebe eine Zahl ein für welche die das 2er komplement haben willst: ");
+
+	int input;
+	scanf_s("%d", &input);
+
+	printf("%d ist negiert %d", input,~(input-1));
+	return 0;
+
+}
